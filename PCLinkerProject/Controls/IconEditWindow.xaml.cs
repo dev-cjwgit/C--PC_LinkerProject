84d3dfa852c45d51cbf568636b00f174a2b05f2a
hyeonslove <cjw.git@gmail.com>
2022-07-26 13:36:46 +0900

    IconEdit Viewer Update

--- PCLinkerProject/Controls/IconEditWindow.xaml.cs
+++ PCLinkerProject/Controls/IconEditWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,18 +21,24 @@ namespace PCLinkerProject.Controls
     /// <summary>
     /// IconEditWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class IconEditWindow : UserControl
+    public partial class IconEditWindow : System.Windows.Controls.UserControl
     {
+        private int selected_index;
         public IconEditWindow()
         {
             InitializeComponent();
             IconEditListbox.ItemsSource = IconEditViewModel.GetInstance().Tabs;
 
 
-            string[] files = Directory.GetFiles(@"C:\File", "*.txt");
+            string[] files = Directory.GetFiles(Environment.CurrentDirectory + @"\ICO\", "*.ico");
             foreach (var file in files)
             {
-                Console.WriteLine(file);
+                String[] split = file.Split('\\');
+                IconEditViewModel.GetInstance().Tabs.Add(new HeaderItem()
+                {
+                    HeaderIcon = Environment.CurrentDirectory + @"\ICO\" + split[split.Length - 1],
+                    HeaderText = split[split.Length - 1]
+                });
             }
 
             //IconEditViewModel.GetInstance().Tabs.Add(new HeaderItem()
@@ -44,6 +51,51 @@ namespace PCLinkerProject.Controls
         private void ProgressBar_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             this.Visibility = Visibility.Collapsed;
+        }
+
+        private void IconEdit_Selected(object sender, SelectionChangedEventArgs e)
+        {
+            dynamic meta_data = sender as dynamic;
+            selected_index = meta_data.SelectedIndex;
+        }
+
+        private void AddButton_onClick(object sender, RoutedEventArgs e)
+        {
+            String[] dir = IconTextbox.Text.Split('\\');
+            string fileName = dir[dir.Length - 1];
+            File.Copy(IconTextbox.Text, Environment.CurrentDirectory + @"\ICO\" + fileName);
+
+            IconEditViewModel.GetInstance().Tabs.Add(new HeaderItem()
+            {
+                HeaderIcon = Environment.CurrentDirectory + @"\ICO\" + fileName,
+                HeaderText = fileName
+            });
+        }
+
+        private void DeleteButton_onClick(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("아직 지원 안합니다.\n직접 폴더에가서 지우고 프로그램을 재실행해주세요.");
+
+            //File.Delete(IconEditViewModel.GetInstance().Tabs[selected_index].HeaderIcon.ToString());
+            //IconEditViewModel.GetInstance().Tabs.RemoveAt(selected_index);
+
+            //selected_index = -1;
+        }
+
+        private void CancelButton_onClick(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Collapsed;
+        }
+
+        private void IconSelectButton_onClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlgOpenFile = new OpenFileDialog();
+            dlgOpenFile.Filter = "Icon File (*.ico) | *.ico;";
+
+            if (dlgOpenFile.ShowDialog().ToString() == "OK")
+            {
+                IconTextbox.Text = dlgOpenFile.FileName;
+            }
         }
     }
 }
