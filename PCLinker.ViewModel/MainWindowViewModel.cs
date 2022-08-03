58ae7e7e6f4eb4d9cec405ce5418cf48ebc4aeaa
hyeonslove <cjw.git@gmail.com>
2022-08-03 14:54:26 +0900

    ContextMenu Command Binding

--- PCLinker.ViewModel/MainWindowViewModel.cs
+++ PCLinker.ViewModel/MainWindowViewModel.cs
@@ -13,7 +13,12 @@ namespace PCLinker.ViewModel
         public MainWindowViewModel()
         {
             Tabs = new ObservableCollection<TabControlHeaderViewModel>();
+            CreateHeaderCommand = new Command(CreateHeader, null);
+            UpdateHeaderCommand = new Command(UpdateHeader, null);
             DeleteHeaderCommand = new Command(DeleteHeader, null);
+
+            CreateContentCommand = new Command(CreateContent, null);
+            UpdateContentCommand = new Command(UpdateContent, null);
             DeleteContentCommand = new Command(DeleteContent, null);
 
             db = new DatabaseManager();
@@ -29,23 +34,45 @@ namespace PCLinker.ViewModel
             }
         }
 
+        private void CreateHeader(object obj)
+        {
+            Console.WriteLine("헤더 추가");
+        }
+
+        private void UpdateHeader(object obj)
+        {
+            Console.WriteLine("헤더 수정");
+        }
 
         private void DeleteHeader(object obj)
         {
-            Console.WriteLine("");
+            Console.WriteLine("헤더 삭제");
+        }
+
+        private void CreateContent(object obj)
+        {
+            Console.WriteLine("컨텐츠 생성");
+        }
+
+        private void UpdateContent(object obj)
+        {
+            Console.WriteLine("컨텐츠 수정");
         }
 
         private void DeleteContent(object obj)
         {
-            Console.WriteLine("");
+            Console.WriteLine("컨텐츠 삭제");
         }
 
         public TabControlHeaderViewModel SelectedHeaderItem { get; set; }
 
         public TabContentViewModel SelectedContentItem { get; set; }
-
+        public ICommand CreateHeaderCommand { get; private set; }
+        public ICommand UpdateHeaderCommand { get; private set; }
         public ICommand DeleteHeaderCommand { get; private set; }
 
+        public ICommand CreateContentCommand { get; private set; }
+        public ICommand UpdateContentCommand { get; private set; }
         public ICommand DeleteContentCommand { get; private set; }
 
         public ObservableCollection<TabControlHeaderViewModel> Tabs { get; set; }
