7792af08199fd6674e77301cc5872cf613c2287e
hyeonslove <cjw.git@gmail.com>
2022-08-07 17:00:09 +0900

    예외처리

--- PCLinker.BusinessModel/IconEditManager.cs
+++ PCLinker.BusinessModel/IconEditManager.cs
@@ -9,22 +9,25 @@ namespace PCLinker.BusinessModel
     {
         public bool CopyIcon(string iconPath)
         {
-
-            String[] dir = iconPath.Split('\\');
-            string fileName = dir[dir.Length - 1];
-            try
+            if (iconPath != "")
             {
-                File.Copy(iconPath, Environment.CurrentDirectory + @"\ICO\" + fileName);
+                String[] dir = iconPath.Split('\\');
+                string fileName = dir[dir.Length - 1];
+                try
+                {
+                    File.Copy(iconPath, Environment.CurrentDirectory + @"\ICO\" + fileName);
 
-                return true;
-            }
-            catch (IOException ex)
-            {
-                if (ex.Message.Contains("파일이 이미 있습니다"))
-                    return false;
+                    return true;
+                }
+                catch (IOException ex)
+                {
+                    if (ex.Message.Contains("파일이 이미 있습니다"))
+                        return false;
 
-                throw ex;
+                    throw ex;
+                }
             }
+            return false;
         }
 
         public bool DeleteIcon(string iconPath)
